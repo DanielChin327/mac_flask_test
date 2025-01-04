86f60f2a8e9e9de6605340ee5c9d3e4705a7f13c
Daniel Chin <danielchin@Daniels-MacBook-Pro.local>
2025-01-04 15:00:03 -0700

    working on backend

--- app.py
+++ app.py
@@ -1,53 +1,37 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 # MySQL Database Connection Configuration for XAMPP
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_todo_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mariadb+pymysql://root:@localhost:3306/mac_test_flask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+
 # Database Model
-class Task(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    complete = db.Column(db.Boolean)
+class Quote(db.Model):
+    quote_id = db.Column(db.Integer, primary_key=True)
+    person = db.Column(db.String(50))
+    quote = db.Column(db.String(255))
 
 # Initialize the Database (Run only once to create the tables)
 with app.app_context():
     db.create_all()
 
 # Home Route - Display tasks
-@app.route("/")
-def home():
-    tasks = Task.query.all()
-    return render_template("index.html", tasks=tasks)
+@app.route('/')
+def testing():
+    return "Flask Routes are Working. "
 
-# Add New Task
-@app.route("/add", methods=["POST"])
-def add():
-    title = request.form.get("title")
-    new_task = Task(title=title, complete=False)
-    db.session.add(new_task)
-    db.session.commit()
-    return redirect(url_for("home"))
+@app.route('/quotes', methods=['GET'])
+def get_quotes():
+    quotes = Quote.query.all()
+    #converts to json format
+    quotes_list = [{"quote_id": q.quote_id, "person": q.person, "quote": q.quote} for q in quotes]
+    return jsonify(quotes_list)
 
-# Toggle Task Completion
-@app.route("/update/<int:task_id>")
-def update(task_id):
-    task = Task.query.get(task_id)
-    task.complete = not task.complete
-    db.session.commit()
-    return redirect(url_for("home"))
-
-# Delete Task
-@app.route("/delete/<int:task_id>")
-def delete(task_id):
-    task = Task.query.get(task_id)
-    db.session.delete(task)
-    db.session.commit()
-    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
